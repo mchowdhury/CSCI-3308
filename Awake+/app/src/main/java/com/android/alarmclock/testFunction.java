c5811f7e69a9820f4d26586b68826ec75b030386
Samuel Skolnekovich <samuelskolnekovich@MacBook-Pro.local>
2015-11-10 19:46:31 -0700

    new file

---
+++ Awake+/app/src/main/java/com/android/alarmclock/testFunction.java
@@ -0,0 +1,57 @@
+package com.android.alarmclock;
+import java.lang.Math;
+import java.util.Date;
+/**
+ * Created by samuelskolnekovich on 11/10/15.
+ */
+public class testFunction {
+    public boolean isnewDay;
+    public boolean isSnooze;
+    public int totalpoints=0;
+    private int points=1;
+    private Date oldDate=new Date();
+    private int counter=0;
+
+    public void test(Date currentDate){
+        //is it a new day
+        if(currentDate==oldDate){
+            counter=counter+1;
+            if(counter==1){
+                isnewDay=true;
+            }
+            else{
+                isnewDay=false;
+            }
+        }
+        if(currentDate!=oldDate){
+            oldDate=currentDate;
+            isnewDay=true;
+            counter=1;
+        }
+
+
+        //Snooze functionality
+        double x =Math.random();
+        if(x<.5){
+            isSnooze=true;
+        }
+        if(x>=.5){
+            isSnooze=false;
+        }
+
+        if(isSnooze==false && isnewDay==true) {
+            totalpoints = totalpoints + points;
+        }
+
+
+    }
+    public boolean getIsSnooze(){
+        return isSnooze;
+    }
+    public boolean getIsNewDay(){
+        return isnewDay;
+    }
+    public boolean getTotalPoints(){
+        return totalpoints;
+    }
+}
