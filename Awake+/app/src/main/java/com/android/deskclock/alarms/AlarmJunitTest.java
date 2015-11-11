9b1b6a3f4002d3b562977c14507d9f5dadb786d8
Mikhail Chowdhury <mchowdhury13@kentdenver.org>
2015-11-10 20:31:15 -0700

    Added initial unit tests

---
+++ Awake+/app/src/main/java/com/android/deskclock/alarms/AlarmJunitTest.java
@@ -0,0 +1,25 @@
+package com.android.deskclock.alarms;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Mikhail on 11/10/2015.
+ */
+public class AlarmJunitTest extends TestCase {
+
+    public void testTest1() throws Exception {
+
+    }
+
+    public void testGetIsSnooze() throws Exception {
+
+    }
+
+    public void testGetIsNewDay() throws Exception {
+
+    }
+
+    public void testGetTotalPoints() throws Exception {
+
+    }
+}
