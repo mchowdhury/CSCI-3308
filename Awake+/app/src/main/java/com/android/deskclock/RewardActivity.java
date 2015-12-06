879e228a970a32e88874b3d19a9fd7ea65a64f29
Samuel Skolnekovich <samuelskolnekovich@MacBook-Pro.local>
2015-12-05 20:31:24 -0700

    Final.

--- Awake+/app/src/main/java/com/android/deskclock/RewardActivity.java
+++ Awake+/app/src/main/java/com/android/deskclock/RewardActivity.java
@@ -1,8 +1,11 @@
 package com.android.deskclock;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class RewardActivity extends Activity {
@@ -14,7 +17,20 @@ public class RewardActivity extends Activity {
 
         TextView points=(TextView) findViewById(R.id.Rpoints);
         String x= DeskClockApplication.getPointsString();
-        points.setText("You have: " + x + "Points");
+        points.setText("You have\n\n" + x + "!");
+
+
+        Button z = (Button) findViewById(R.id.rew);
+
+        z.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ((DeskClockApplication) this.getApplication()).addPoints(-((DeskClockApplication) this.getApplication()).getPoints());
+                TextView points=(TextView) findViewById(R.id.Rpoints);
+                points.setText("Reward Redeemed!");
+            }
+        });
 
     }
+
 }
